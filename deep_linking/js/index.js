2a986c721272c6c5fcb11b3b10e140fab3dc6363
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2021-10-29 16:59:31 +0600

    3

--- deep_linking/js/index.js
+++ deep_linking/js/index.js
@@ -1,5 +1,24 @@
-try {
-    window.location.replace("atmkz://")
-} catch (error) {
-    window.alert(JSON.stringify(error))
+
+window.onload = function () {
+
+    function getOrderId() {
+        const query = window.location.search.substring(1);
+        const vars = query.split('&');
+        if (vars) {
+            const orderId = vars[0].split("=")[1];
+            return orderId
+        } else {
+            return null
+        }
+    }
+
+    const orderId = getOrderId()
+
+    if (orderId) {
+        window.location.replace(`atmkz://orders/?orderId=${orderId}`)
+    }
+
 }
+
+
+
