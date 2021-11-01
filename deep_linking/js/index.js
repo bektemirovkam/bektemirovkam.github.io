cc85b16a7032c93d2f7132098e90cf1f007d561f
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2021-11-01 08:48:12 +0600

    9

--- deep_linking/js/index.js
+++ deep_linking/js/index.js
@@ -42,7 +42,7 @@ window.onload = function () {
             window.location.replace(`intent:#Intent;scheme=atmkz://test/orders/?orderId=${orderId};package=com.electronic.electronic;end`)
             // window.location.replace(`intent:#Intent;package=com.profit.patientcab;end`)
         } else if (currentSystem === "iOS") {
-            window.location.replace(`atmkz://orders/?orderId=${orderId}`)
+            window.location.replace(`atmkz://test/orders/?orderId=${orderId}`)
         }
     }
 
